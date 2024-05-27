e30bf72a8d278d6411cc484f497af0a2d71fe5a4
usa3amustafa <usa3a3ustafa@outlook.com>
2024-05-27 16:21:40 +0500

    update

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -1,57 +1,22 @@
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
-import LocomotiveScroll from "locomotive-scroll"
 import PropTypes from "prop-types"
 
-const ScrollToTop = ({ options, children }) => {
+const ScrollToTop = ({ children }) => {
   const { pathname } = useLocation()
-  const scrollRef = useRef(null)
 
   useEffect(() => {
-    if (scrollRef.current) {
-      const scroll = new LocomotiveScroll({
-        el: scrollRef.current,
-        smooth: true,
-        lerp: options.lerp || 0.5, // Adjust lerp for scroll smoothness
-        multiplier: options.multiplier || 1, // Adjust scroll speed
-        smoothMobile: true, // Ensure smooth scrolling on mobile as well
-        getSpeed: true,
-        getDirection: true,
-        tablet: {
-          smooth: true,
-          multiplier: options.multiplier || 1,
-        },
-        smartphone: {
-          smooth: true,
-          multiplier: options.multiplier || 1,
-        },
-      })
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    })
+  }, [pathname])
 
-      // Update the scroll container height
-      scroll.update()
-
-      // Scroll to top on route change
-      scroll.scrollTo(0, { duration: options.scrollToTopDuration || 1500 })
-
-      return () => {
-        scroll.destroy()
-      }
-    }
-  }, [pathname, options])
-
-  return (
-    <div data-scroll-container ref={scrollRef}>
-      {children}
-    </div>
-  )
+  return children
 }
 
 ScrollToTop.propTypes = {
-  options: PropTypes.shape({
-    lerp: PropTypes.number,
-    multiplier: PropTypes.number,
-    scrollToTopDuration: PropTypes.number,
-  }),
   children: PropTypes.node.isRequired,
 }
 
